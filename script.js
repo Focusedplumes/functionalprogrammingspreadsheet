4b39d505207ed8034d215dc6c53c10f614de17a3
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-04-22 21:49:02 -0500

    testing that it gets uploaded

--- script.js
+++ script.js
@@ -6,9 +6,18 @@ const median = nums => {
     const sorted = nums.slice().sort((a, b) => a - b);
     const length = sorted.length
     const middle = length / 2 - 1;
+    return isEven(length)
+    ? average([sorted[middle], sorted[middle + 1]])
+    : sorted[Math.ceil(middle)];
   }
 
-const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
+const spreadsheetFunctions = {
+  sum,
+  average,
+  median,
+}
+
+  const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
 
 window.onload = () => {
@@ -28,7 +37,14 @@ window.onload = () => {
     input.type = "text";
     input.id = letter + number;
     input.ariaLabel = letter + number;
+    input.onchange = update;
     container.appendChild(input);
     });
     })
     }
+    const update = event => {
+    const element = event.target;
+    const value = element.value.replace(/\s/g, "");
+    }
+    
+    
